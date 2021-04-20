c0c60ffe83adb55a6dedefa28ca07f21fb75ef78
David Enciso <dpenciso@msn.com>
2021-04-19 20:05:53 -0600

    seed script

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,61 +2,28 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-const userData = [
+const characterData = [
   {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
-    },
+    name: 'Link',
+    description: 'A Hyrule warrior',
+    gender: 'Male',
+    race: 'Hylian',
   },
   {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-          viewCount: 42,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-          viewCount: 128,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
+    name: 'Zelda',
+    description: 'A Hyrule princess',
+    gender: 'Female',
+    race: 'Hylian',
   },
 ]
 
 async function main() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
+  for (const c of characterData) {
+    const character = await prisma.character.create({
+      data: c,
     })
-    console.log(`Created user with id: ${user.id}`)
+    console.log(`Created character with id: ${character.id}`)
   }
   console.log(`Seeding finished.`)
 }
